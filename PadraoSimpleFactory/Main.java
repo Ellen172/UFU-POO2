a4160df739376359fab9bd0b18fcb38fd7ad091e
EllenCas172 <ellencas172@gmail.com>
2022-03-06 15:10:14 -0300

    padrao simplefactory

--- PadraoSimpleFactory/Main.java
+++ PadraoSimpleFactory/Main.java
@@ -2,5 +2,15 @@
 
 public class Main
 {
-    Personagem p = new SimplePersonagemFactory();
+    public static void main(String args[]){
+        
+        Personagem mago = SimplePersonagemFactory.createMago();
+        Personagem feiticeiro = SimplePersonagemFactory.createFeiticeiro();
+        Personagem campones = SimplePersonagemFactory.createCampones();
+        
+        // gera numeros de 1 a 100
+        int random = 1 + (int) (Math.random() * 100);
+        Personagem p = SimplePersonagemFactory.createPersonagem(random);
+        
+    }
 }
--- PadraoSimpleFactory/SimplePersonagemFactory.java
+++ PadraoSimpleFactory/SimplePersonagemFactory.java
@@ -2,22 +2,31 @@ public class SimplePersonagemFactory{
     // para criar aleatoriamente
     public static Personagem createPersonagem(int tipo){
         Personagem p = null;
-        if (tipo == 1) 
+        if (tipo > 60) {
             p = new Mago();
-        else if (tipo > 0.5) 
+            System.out.println("Novo Mago");
+        }
+        else if (tipo > 30) {
             p = new Feiticeiro();
-        else
-            p = new Campones(); 
+            System.out.println("Novo Feiticeiro");
+        }
+        else {
+            p = new Campones();
+            System.out.println("Novo Camponês");
+        }
         return p;
     }
     // para criar especificos
     public static Personagem createMago(){
+        System.out.println("Novo Mago");
         return new Mago();
     }
     public static Personagem createFeiticeiro(){
+        System.out.println("Novo Feiticeiro");
         return new Feiticeiro();
     }
     public static Personagem createCampones(){
+        System.out.println("Novo Camponês");
         return new Campones(); 
     }
 }  
